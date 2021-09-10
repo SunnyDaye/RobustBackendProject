954cdaa4d521c1693d0b8929e8a8c371032a507f
Suncerie Daye <sunceried@gmail.com>
2021-09-10 14:31:56 -0700

    Environment bug fixed

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || "production";
+const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment]; //Grabbing the configuration object based on the env
 const knex = require("knex")(config);
 
